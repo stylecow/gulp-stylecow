bc98ac4cb58a02d2fab27fd743a06473ccbe6497
oscarotero <oom@oscarotero.com>
2014-11-16 12:12:30 +0100

    source-map implementation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var through = require('through2');
 var stylecow = require('stylecow');
 var gutil = require('gulp-util');
+var applySourceMap = require('vinyl-sourcemaps-apply');
 var PluginError = gutil.PluginError;
+var path = require('path');
 
 module.exports = function (options) {
   stylecow.setConfig(options);
@@ -10,13 +12,28 @@ module.exports = function (options) {
     if (file.isNull()) return cb(null, file); 
     if (file.isStream()) return cb(new PluginError('gulp-stylecow', 'Streaming not supported'));
 
+    stylecow.cwd(path.dirname(file.path));
+
     try {
       var css = stylecow.convert(file.contents.toString('utf8'));
+
+      if (file.sourceMap && file.sourceMap.sources.length) {
+        css.setData('sourceFile', file.relative);
+      }
     } catch (err) {
       return cb(new PluginError('gulp-stylecow', err));
     }
 
-    file.contents = new Buffer(css.toCode());
+    var code = new stylecow.Code(css, {
+      style: options.code,
+      sourceMap: file.sourceMap ? true : false
+    });
+
+    if (code.map && file.sourceMap) {
+      applySourceMap(file, code.map.toString());
+    }
+
+    file.contents = new Buffer(code.code);
     cb(null, file);
   }
 
