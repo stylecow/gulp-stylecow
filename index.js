1e4c019e27eabe25ed3e59f98781e9ad75d17322
oscarotero <oom@oscarotero.com>
2015-11-22 12:00:14 +0100

    added src function to resolve glob files

--- index.js
+++ index.js
@@ -6,60 +6,88 @@ var through        = require('through2'),
     gutil          = require('gulp-util'),
     applySourceMap = require('vinyl-sourcemaps-apply'),
     PluginError    = gutil.PluginError,
+    glob           = require('glob'),
     path           = require('path');
 
 module.exports = function (config) {
-  let tasks = new stylecow.Tasks();
-  let coder = new stylecow.Coder(config.code);
+    let tasks = new stylecow.Tasks();
+    let coder = new stylecow.Coder(config.code);
 
-  if (config.support) {
-      tasks.minSupport(config.support);
-  }
-
-  tasks.use(plugins(config.plugins));
-
-  if (config.modules) {
-      config.modules.forEach(function (module) {
-          tasks.use(require(module));
-      });
-  }
-
-  function transform (file, enc, cb) {
-    if (file.isNull()) {
-      return cb(null, file);
+    if (config.support) {
+        tasks.minSupport(config.support);
     }
 
-    if (file.isStream()) {
-      return cb(new PluginError('gulp-stylecow', 'Streaming not supported'));
+    tasks.use(plugins(config.plugins));
+
+    if (config.modules) {
+        config.modules.forEach(function (module) {
+            tasks.use(require(module));
+        });
     }
 
-    let css, code, map;
+    function transform (file, enc, cb) {
+        if (file.isNull()) {
+            return cb(null, file);
+        }
 
-    try {
-      css = stylecow.parse(file.contents.toString('utf8'), 'Root', null, file.path);
-      tasks.run(css);
-    } catch (err) {
-      return cb(new PluginError('gulp-stylecow', err));
+        if (file.isStream()) {
+            return cb(new PluginError('gulp-stylecow', 'Streaming not supported'));
+        }
+
+        let css, code, map;
+
+        try {
+            css = stylecow.parse(file.contents.toString('utf8'), 'Root', null, file.path);
+            tasks.run(css);
+        } catch (err) {
+            return cb(new PluginError('gulp-stylecow', err));
+        }
+
+        if (file.sourceMap) {
+            coder.sourceMap('none');
+            code = coder.run(css, file.relative, file.sourceMap);
+
+            map = JSON.parse(code.map);
+            map.file = file.sourceMap.file;
+            applySourceMap(file, map);
+        } else {
+            if (config.map) {
+                coder.sourceMap(config.map);
+            }
+
+            code = coder.run(css);
+        }
+
+        file.contents = new Buffer(code.css);
+        cb(null, file);
     }
 
-    if (file.sourceMap) {
-      coder.sourceMap('none');
-      code = coder.run(css, file.relative, file.sourceMap);
-
-      map = JSON.parse(code.map);
-      map.file = file.sourceMap.file;
-      applySourceMap(file, map);
-    } else {
-      if (config.map) {
-        coder.sourceMap(config.map);
-      }
-
-      code = coder.run(css);
-    }
-
-    file.contents = new Buffer(code.css);
-    cb(null, file);
-  }
-
-  return through.obj(transform);
+    return through.obj(transform);
 };
+
+module.exports.src = function(files) {
+    let resolved = [];
+
+    files.forEach(function (file) {
+        if (!glob.hasMagic(file.input)) {
+            return resolved.push(file);
+        }
+
+        glob
+            .sync(file.input)
+            .forEach(function (input) {
+                let info = path.parse(input);
+
+                resolved.push({
+                    input: input,
+                    output: file.output
+                        .replace('[dir]', info.dir)
+                        .replace('[base]', info.base)
+                        .replace('[ext]', info.ext)
+                        .replace('[name]', info.name)
+                });
+            });
+    });
+
+    return resolved;
+}
--- test/stylecow_test.js
+++ test/stylecow_test.js
@@ -1,34 +1,36 @@
-var stylecow = require('../');
-var gutil = require('gulp-util');
-var fs = require('fs');
-var path = require('path');
-var assert = require('assert');
+"use strict"
+
+let stylecow = require('../'),
+    gutil    = require('gulp-util'),
+    fs       = require('fs'),
+    path     = require('path'),
+    assert   = require('assert');
 
 function createVinyl (cssFile, contents) {
-  var base = path.join(__dirname, 'fixtures');
-  var filePath = path.join(base, cssFile);
+    let base     = path.join(__dirname, 'fixtures'),
+        filePath = path.join(base, cssFile);
 
-  return new gutil.File({
-    cwd: __dirname,
-    base: base,
-    path: filePath,
-    contents: contents || fs.readFileSync(filePath)
-  });
+    return new gutil.File({
+        cwd: __dirname,
+        base: base,
+        path: filePath,
+        contents: contents || fs.readFileSync(filePath)
+    });
 }
 
 describe('gulp-stylecow', function () {
-	describe('stylecow()', function () {
-		it('should match fixtures/styles.css and expected/styles.css', function (done) {
-			var input = createVinyl('styles.css');
-			var stream = stylecow(require('./fixtures/stylecow.json'));
+    describe('stylecow()', function () {
+        it('should match fixtures/styles.css and expected/styles.css', function (done) {
+            let input  = createVinyl('styles.css'),
+                stream = stylecow(require('./fixtures/stylecow.json'));
 
-			stream.once('data', function (cssFile) {
-				assert.equal(cssFile.contents.toString(), fs.readFileSync(path.join(__dirname, 'expected/styles.css'), 'utf8'));
-				done();
-			});
+            stream.once('data', function (cssFile) {
+                assert.equal(cssFile.contents.toString(), fs.readFileSync(path.join(__dirname, 'expected/styles.css'), 'utf8'));
+                done();
+            });
 
-			stream.write(input);
-			stream.end();
-		});
-	});
+            stream.write(input);
+            stream.end();
+        });
+    });
 });
